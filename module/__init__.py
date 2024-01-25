a603e3d8b30c165b709a517edc96625f7f8cc9b8
Hlib Shyn <gshin907@gmail.com>
2024-01-26 00:35:51 +0200

    add migrations

--- module/__init__.py
+++ module/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py', silent=True)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 import module.views
 import module.models
